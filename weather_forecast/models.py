19e15cd7b3b39a560871886b5f5c9c8e4ab5cdc3
vavilion2 <tima228007@gmail.com>
2021-10-15 14:29:39 +0300

    Fixed(admin):now you can see time in the model instance(with docker)

--- weather_forecast/models.py
+++ weather_forecast/models.py
@@ -5,7 +5,6 @@ class WeatherInfo(models.Model):
     city = models.CharField(max_length=50,blank=True,null=True)
     temperature= models.CharField(max_length=23,blank=True,null=True)
     humidity=models.CharField(max_length=50,blank=True,null=True)
-
     date = models.DateTimeField(default=now)
 
     pass
